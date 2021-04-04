1736e9e05ffc8f8a6fd68dd8e3a601b76b4355f4
Callum Mackenzie <Alexxander1611@gmail.com>
2021-04-04 15:25:14 -0600

    Shader class work.

--- src/ingenium/world/Shader.java
+++ src/ingenium/world/Shader.java
@@ -1,10 +1,15 @@
 package ingenium.world;
 
+import java.nio.IntBuffer;
+
+import com.jogamp.opengl.GL2;
+
 public class Shader {
     private int program = -1;
 
-    public Shader() {
-
+    public Shader(GL2 gl, String vertSource, String fragSource) {
+        int vShader = GL2.GL_NONE, fShader = GL2.GL_NONE;
+        program = gl.glCreateProgram();
     }
 
     public int getProgram() {
@@ -13,5 +18,13 @@ public class Shader {
 
     public void setProgram(int program) {
         this.program = program;
+    }
+
+    public static int compileShader(GL2 gl, int type, String source) {
+        int shader = gl.glCreateShader(type);
+        String src[] = { source };
+        gl.glShaderSource(shader, 1, src, (int[]) null, 0);
+        gl.glCompileShader(shader);
+        return shader;
     }
 }
